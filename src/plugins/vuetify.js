e0206f1a88a174ffa0ad009ff25f8d0f1b344d15
Pablo Bender <pablo@bender.eti.br>
2019-03-13 18:20:08 -0400

    simple v-card app

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify, {
+  VApp, // required
+  VCard,
+  VCardTitle,
+  VCardText
+} from 'vuetify/lib'
+
 import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
+  components: {
+    VApp, // required
+    VCard,
+    VCardTitle,
+    VCardText
+  },
   iconfont: 'md',
 })
